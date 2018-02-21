abb545045f3ec4b673b5eab83312c457a7ac2f8b
KirylNik <ultramylo@gmail.com>
2018-02-22 00:51:18 +0300

    done 56%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let temp =  0;
+    let result = 0;
+    temp = Math.floor(number / 5);
+    result += temp;
+    temp = Math.floor(number / 25);
+    result += temp;
+    temp = Math.floor(number / 125);
+    result += temp;
+    temp = Math.floor(number / 625);
+    result += temp;
+    temp = Math.floor(number / 3125);
+    result += temp;
+    temp = Math.floor(number / 15625);
+    result += temp;
+    temp = Math.floor(number / 78125);
+    result += temp;
+    temp = Math.floor(number / 390625);
+    result += temp;
+    temp = Math.floor(number / 1953125);
+    result += temp;
+    temp = Math.round(number / 9765625);
+    result += temp;
+    if (1100000 < result && result < 7500000) {
+        result = result - 1;
+    }
+    return(Math.round (result));
 }
